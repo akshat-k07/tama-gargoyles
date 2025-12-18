06efb65cc41bae5bb1df96a8739f1759a7379388
Isaac Madgwick <isaacmadgwick@gmail.com>
2025-12-18 11:03:25 +0000

    Added user model tests

---
+++ src/test/java/com/example/tama_gargoyles/model/UserTest.java
@@ -0,0 +1,48 @@
+package com.example.tama_gargoyles.model;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class UserTest {
+
+    @Test
+    public void testConstructor(){
+
+        String user_email = "test@email.com";
+        User testUser = new User(user_email);
+
+        assertEquals(user_email, testUser.getEmail());
+        assertEquals("null", testUser.getUsername());
+        assertEquals(0L, testUser.getBugs().longValue());
+        assertEquals(0L, testUser.getRocks().longValue());
+        assertEquals(0L, testUser.getMystery_food().longValue());
+
+    }
+
+    @Test
+    public void testLombokGetAndSet(){
+
+        User testUser = new User();
+        testUser.setBugs(2);
+        testUser.setRocks(65);
+        testUser.setMystery_food(1);
+        testUser.setEmail("test@email.com");
+
+        assertEquals("test@email.com", testUser.getEmail());
+        assertEquals(2L, testUser.getBugs().longValue());
+        assertEquals(65L, testUser.getRocks().longValue());
+        assertEquals(1L, testUser.getMystery_food().longValue());
+
+    }
+
+    @Test
+    public void testEquals(){
+
+        User user1 = new User("test@email.com");
+        User user2 = new User("test@email.com");
+
+        assertEquals(user1, user2);
+
+    }
+
+}
