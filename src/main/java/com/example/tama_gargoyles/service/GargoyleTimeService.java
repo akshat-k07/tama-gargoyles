41cb87a37a080e30fd138099828eb73c3080c9cf
David Lloyd <deltanet@mac.com>
2025-12-19 15:42:58 +0000

    added comments

--- src/main/java/com/example/tama_gargoyles/service/GargoyleTimeService.java
+++ src/main/java/com/example/tama_gargoyles/service/GargoyleTimeService.java
@@ -17,6 +17,8 @@ public class GargoyleTimeService {
     public static final int HAPPINESS_DECAY_PER_MIN = 3;
     public static final int HEALTH_DECAY_PER_MIN = 10;
 
+    // These below are currently not functional - but could be called into play
+    // if we want to have a logic where stats increase with the passage of time.
     public static final int HUNGER_RAISE_PER_MIN = 2;
     public static final int HAPPINESS_RAISE_PER_MIN = 3;
     public static final int HEALTH_RAISE_PER_MIN = 10;
@@ -47,6 +49,7 @@ public class GargoyleTimeService {
         // Core decay
         int hungerDrop = (int) (minutes * HUNGER_DECAY_PER_MIN);
         int happinessDrop = (int) (minutes * HAPPINESS_DECAY_PER_MIN);
+        // Health does not decay with time unless other conditions are met
         int healthDrop = (int) (0 * HEALTH_DECAY_PER_MIN);
 
         // Simple “emergent” rule: if hungry, happiness falls faster.
@@ -59,12 +62,13 @@ public class GargoyleTimeService {
             healthDrop += minutes * 1; // extra -1 per minute
         }
 
-        // Simple “emergent” rule: if hunger and happiness are zero, health falls rapidly.
+        // Simple “emergent” rule: if hunger and happiness are  near zero, health falls rapidly.
         if (game.getHunger() < 10 && game.getHappiness() < 10) {
             healthDrop += minutes * 3; // extra -3 per minute
         }
 
         // Core Health improvement
+        // These don't do anything yet - see the comment at the beginning of the file
         int hungerRaise = (int) (0 * HUNGER_RAISE_PER_MIN);
         int happinessRaise = (int) (0 * HAPPINESS_RAISE_PER_MIN);
         int healthRaise = (int) (0 * HEALTH_RAISE_PER_MIN);
@@ -80,6 +84,7 @@ public class GargoyleTimeService {
             healthRaise += minutes * 3; // extra +3 per minute
         }
 
+        // the values for hunger, happiness and health take into account both the drop and the raise
         game.setHunger(clamp01to100(game.getHunger() - hungerDrop + hungerRaise));
         game.setHappiness(clamp01to100(game.getHappiness() - happinessDrop + happinessRaise));
         game.setHealth(clamp01to100(game.getHealth() - healthDrop + healthRaise));
