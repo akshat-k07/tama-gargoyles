e50b6cbc7c0100c4fac3c9097f1db24641a0c1b7
Akshat Khandelwal <akshatlinked20@gmail.com>
2026-01-05 13:15:14 +0000

    Implemented feature for mystery food

--- src/main/java/com/example/tama_gargoyles/controller/GargoyleController.java
+++ src/main/java/com/example/tama_gargoyles/controller/GargoyleController.java
@@ -22,6 +22,7 @@ import org.springframework.security.core.Authentication;
 
 
 import java.util.Date;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -82,13 +83,27 @@ public class GargoyleController {
 
     // Currently thymeleaf just gives zero values, need to program for randomisation
     @PostMapping("/mystery-increase")
-    public RedirectView increaseMystery(@RequestParam Integer strengthDelta, @RequestParam Integer speedDelta,
-                                       @RequestParam Integer intelligenceDelta, @RequestParam Integer hungerDelta, @RequestParam Long gargoyleId){
+    public RedirectView increaseMystery(@RequestParam Long gargoyleId){
+        Random randomNum = new Random();
+        int toIncrease = randomNum.nextInt(3);
         Gargoyle gargoyle = gargoyleRepository.findById(gargoyleId).get();
-        gargoyle.setStrength(Math.max(0, Math.min(gargoyle.getStrength() + strengthDelta, 100)));
-        gargoyle.setSpeed(Math.max(0, Math.min(gargoyle.getSpeed() + speedDelta, 100)));
-        gargoyle.setIntelligence(Math.max(0, Math.min(gargoyle.getIntelligence() + intelligenceDelta, 100)));
-        gargoyle.setHunger(Math.max(0, Math.min(gargoyle.getHunger() + hungerDelta, 100)));
+        if (toIncrease == 0){
+            gargoyle.setStrength(Math.max(0, Math.min(gargoyle.getStrength() + randomNum.nextInt(1, 25), 100)));
+            gargoyle.setSpeed((Math.min(100, Math.max(gargoyle.getSpeed() - randomNum.nextInt(1, 7), 0))));
+            gargoyle.setIntelligence((Math.min(100, Math.max(gargoyle.getIntelligence() - randomNum.nextInt(1, 7), 0))));
+        }
+        else if (toIncrease == 1){
+            gargoyle.setSpeed(Math.max(0, Math.min(gargoyle.getSpeed() + randomNum.nextInt(7, 25), 100)));
+            gargoyle.setStrength((Math.min(100, Math.max(gargoyle.getStrength() - randomNum.nextInt(1, 7), 0))));
+            gargoyle.setIntelligence((Math.min(100, Math.max(gargoyle.getIntelligence() - randomNum.nextInt(1, 7), 0))));
+        }
+        else{
+            gargoyle.setIntelligence(Math.max(0, Math.min(gargoyle.getIntelligence() + randomNum.nextInt(1, 25), 100)));
+            gargoyle.setSpeed((Math.min(100, Math.max(gargoyle.getSpeed() - randomNum.nextInt(1, 7), 0))));
+            gargoyle.setStrength((Math.min(100, Math.max(gargoyle.getStrength() - randomNum.nextInt(1, 7), 0))));
+        }
+        gargoyle.setHunger(100);
+
         gargoyleRepository.save(gargoyle);
         return new RedirectView("/game");
     }
